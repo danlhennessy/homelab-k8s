0ad75e64164806c26270532150ebc1493b02b55d
danlhennessy <danh118@hotmail.co.uk>
2023-07-29 22:09:31 +0100

    detail

--- Scripts/k8s/mgmt-cluster/main.go
+++ Scripts/k8s/mgmt-cluster/main.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
+	// Grab secrets from AZ Key Vault
 	azCreds, _ := Get_Secret("azure-credentials")
 	sv_principal, _ := Get_Secrets([]string{"aks-clientSecret", "aks-clientID"})
 
+	// Push secrets to Kubernetes (Current kubeconfig context)
 	Apply_String_Secret("crossplane-system", "azure-secret", "creds", azCreds)
 	Apply_Multi_String_Secret("default", "sv-principal", sv_principal)
 	// Apply_JSON_Secret("default", "json-test", azCreds)
