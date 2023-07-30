710bdf980d84ee9cb1e0aa249d55b22ea0498091
danlhennessy <danh118@hotmail.co.uk>
2023-07-30 09:18:54 +0100

    apply string array of manifests

--- Scripts/k8s/mgmt-cluster/main.go
+++ Scripts/k8s/mgmt-cluster/main.go
@@ -9,4 +9,9 @@ func main() {
 	Apply_String_Secret("crossplane-system", "azure-secret", "creds", azCreds)
 	Apply_Multi_String_Secret("default", "sv-principal", sv_principal)
 	// Apply_JSON_Secret("default", "json-test", azCreds)
+	manifests := []string{
+		"/media/dan/Transcend9/Backup/Work/DevOps/homelab/Kubernetes/manifests/apache/apache.yaml",
+		"/media/dan/Transcend9/Backup/Work/DevOps/homelab/Kubernetes/manifests/apache/service.yaml",
+	}
+	Apply_Manifests(manifests)
 }
